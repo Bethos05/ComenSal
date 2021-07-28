bf2ab915ba695d8335461533af8450355e8010e9
Alberto Alvarez Herrera - Ceiba Software <alberto.alvarez@ceiba.com.co>
2021-07-27 23:51:47 -0500

    add schema data base

---
+++ microservicio/src/main/resources/db/migration/DDL/V2.0__schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS RESTAURANTE (
+          id SERIAL,
+          nombre VARCHAR(255) NOT NULL,
+          precio_reserva DECIMAL NOT NULL,
+          PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS MESA (
+          id SERIAL,
+          id_restaurante INTEGER NOT NULL,
+          estado_actual BOOLEAN  DEFAULT FALSE,
+          PRIMARY KEY (id, id_restaurante),
+          FOREIGN KEY (id_restaurante) REFERENCES RESTAURANTE(id)
+);
+
+CREATE TABLE IF NOT EXISTS DESCUENTO (
+          id SERIAL unique,
+          codigo INTEGER NOT NULL,
+          id_restaurante INTEGER NOT NULL,
+          valor_descuento DECIMAL NOT NULL,
+          PRIMARY KEY(codigo,id_restaurante),
+          FOREIGN KEY (id_restaurante) REFERENCES RESTAURANTE(id)
+);
+
+CREATE TABLE IF NOT EXISTS RESERVA (
+          id SERIAL,
+          dia_reserva DATE NOT NULL,
+          id_restaurante INTEGER NOT NULL ,
+          mesa_id INTEGER NOT NULL,
+          codigo_descuento INTEGER NOT NULL,
+          precio_reserva DECIMAL NOT NULL,
+          PRIMARY KEY (id),
+          FOREIGN KEY (id_restaurante, mesa_id) REFERENCES MESA(id_restaurante,id),
+          FOREIGN KEY (id_restaurante,codigo_descuento) REFERENCES DESCUENTO(id_restaurante,codigo)
+);
